ef2f965071d5fc43ae11500f0affc42a8a914030
Alexander Nesterov <alex18nest@icloud.com>
2024-06-27 17:36:08 -0300

    Initial Commit

---
+++ Month/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Month
+//
+//  Created by Александр Нестеров on 27/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
